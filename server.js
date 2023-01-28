41679fd7d3c6543afbcb54caf65ed5e529b60dad
chae030 <ychae1997@gmail.com>
2023-01-29 02:47:37 +0900

    웹서버 열어보기

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
 // 서버 띄우기 위한 기본세팅 완료 (express 라이브러리)
+
+app.listen(8080, function() {
+  console.log('listening on 8080');
+});
+// listen(서버 띄울 포토번호, 띄운 후 실행할 코드)
+// 8080 port로 웹서버를 열고, 잘 열리면 터미널에 listening on 8080을 출력해주세요. (http://localhost:8080/)
